435220cdd40818a1571b6d46c5a0276ade316761
Abu Sayed <83999377+abusayed310278@users.noreply.github.com>
2025-06-20 23:58:12 +0600

    add middleware and complete auth

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 
@@ -8,6 +8,13 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json({ limit: "5mb" })); // to parse req.body
+// limit shouldn't be too high to prevent DOS
+app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
+
+app.use(cookieParser());
+
 
 app.use("/api/auth", authRoutes);
 
